0acc9766612d01270eb462161078652c805f1637
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-03-01 14:21:43 +0100

    create login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginformComponent } from './loginform/loginform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
